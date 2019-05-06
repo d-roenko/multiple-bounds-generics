03a9aad3040d69c3073510664dfb6a9bdc007cdb
Dmitry Roenko <d.roenko@qiwi.com>
2019-05-06 17:46:28 +0300

    minimal reproducible example

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,46 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	interface A {
+		void doA();
+	}
+
+	interface B {
+		void doB();
+	}
+
+	@Service
+	static class SomeService {
+		@Autowired
+		public <T extends A & B> SomeService(List<T> impls) {
+			impls.forEach(s -> {
+				s.doA();
+				s.doB();
+			});
+		}
+	}
+
+	@Component
+	static class AwithB implements A, B {
+
+		@Override
+		public void doA() {
+
+		}
+
+		@Override
+		public void doB() {
+
+		}
+	}
+
+	@Component
+	static class AwithoutB implements A {
+
+		@Override
+		public void doA() {
+
+		}
+	}
 }
+
